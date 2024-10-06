46dadfa306685cf98fe4cfb5aceb86ff929f9d44
Abhinav Singh <abhinav.singh01@credgenics.com>
2024-10-06 19:52:32 +0530

    Add solution for pallindrome-number problem

---
+++ leetcode/pallindrome-number.py
@@ -0,0 +1,9 @@
+"""
+Problem statement link
+https://leetcode.com/problems/palindrome-number
+"""
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        str_x = str(x)
+        return str_x == str_x[::-1]
